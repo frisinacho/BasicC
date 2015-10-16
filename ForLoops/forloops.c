39600c4360f0e73267dab5f8ca9756421a548626
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-16 11:26:24 +0200

    You can also do functions inside the loop

--- ForLoops/forloops.c
+++ ForLoops/forloops.c
@@ -3,8 +3,11 @@
 
 int main(int argc, const char * argv[])
 {
+    int y;
     for(int x=0; x<10; x++)
     {
+        y = pow(x,2);
         printf("The counter X has the value: %d\n", x);
+        printf("X^2 = %d\n", y);
     }
 }
